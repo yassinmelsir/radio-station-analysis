260ad0902cae0ed9e20304d57a3bd2a21fe471f5
Yassin Magdi Elsir <yme@MBP-von-Yassin.fritz.box>
2023-07-18 11:52:02 +0200

    functionality finished, w/o error testig

--- production.py
+++ production.py
@@ -1,6 +1,5 @@
 import tkinter as tk
 from tkinter import filedialog
-from matplotlib.figure import Figure
 from matplotlib.backends.backend_tkagg import FigureCanvasTkAgg
 from pymongo import MongoClient
 import pandas as pd
@@ -13,7 +12,7 @@ class GUI:
         self.data = data
         self.txAntennaFilePath = None
         self.txParamsFilePath = None
-        self.rows = []
+        self.rows = None
         self.columns = []
         
         #gui init
@@ -21,19 +20,18 @@ class GUI:
         self.root.title('Production')
         
         #gui elements
-        self.canvas = tk.Canvas(self.root, width=400, height=300)
+        self.figure = plt.figure(figsize=(10,8))
+        self.ax = self.figure.add_subplot(111)
+        self.canvas = tk.Canvas(self.figure, self.root)
+        self.canvas.draw()
         self.text =  tk.Text(self.root)
         self.txAntennaButton = tk.Button(self.root, text='Find TxAntenna File', command=self.__update_antenna_filePath)
         self.txParamsButton = tk.Entry(self.root, text='Find TxParams File', command=self.__update_params_filePath)
         self.load_csv_button = tk.Button(self.root,text='Load From CSV', command=self.__load_from_csv)
         self.load_database_button = tk.Button(self.root, text='Load From Database', command=self.__load_from_database)
         self.save_database_button = tk.Button(self.root, text='Save To Database', command=self.__save_to_database)
-        self.row_entry_var = tk.StringVar()
-        self.row_entry = tk.Entry(self.root, textvariable=self.row_entry_var)
-        self.row_entry.bind("<KeyRelease>", self.__update_rows)
-        # populate statistics button
-        # populate correlation graph button
-        # populate location-correlation graph button
+        self.correlation_graph_button = tk.Button(self.root, text='Standard Correlation Graph', command=self.__populate_correlation_graph)
+        self.location_correl_graph_button = tk.Button(self.root, text='Location Correlation Graph', command=self.__populate_location_correlation_graph)
             
         #gui layout
         self.canvas.grid(row=0,column=0)
@@ -41,11 +39,18 @@ class GUI:
         widgets = [self.txAntennaButton, self.txParamsButton, self.load_csv_button, self.load_database_button, self.save_database_button, self.row_entry]
         for i, widget in widgets:
             widget.grid(row=i, collumn=1)
-        self.checkbuttons = {}
+        self.rowbuttons = {}
+        for i, row in enumerate(self.data.get_multiplexes()):
+            self.rowbuttons[row] = tk.BooleanVar() 
+            checkbutton = tk.Checkbutton(self.root, text=row, variable=self.rowbuttons[row], command=self.__update_rows)
+            row =  i + len(widgets)
+            checkbutton.grid(row=row, column=1)   
+        self.columnbuttons = {}
         for i, column in enumerate(self.data.get_df()):
-            self.checkbuttons[column] = tk.BooleanVar() 
-            checkbutton = tk.Checkbutton(self.root, text=column, variable=self.checkbuttons[column], command=self.__update_columns)
-            checkbutton.grid(row=(i+len(widgets)), column=1)        
+            self.columnbuttons[column] = tk.BooleanVar() 
+            checkbutton = tk.Checkbutton(self.root, text=column, variable=self.columnbuttons[column], command=self.__update_columns)
+            row = i + len(widgets) + len(self.data.get_multiplexes())
+            checkbutton.grid(row=row, column=1)        
         
         self.roof.mainloop()
         
@@ -70,11 +75,10 @@ class GUI:
     def __update_rows(self,event):
         #check and prompt for correct input
         self.rows.clear()
-        try:
-            rows = int(self.row_entry.get().split(' ')) # update validation
-        except:
-            print('Entry Not in Range') # show to window
-        self.rows = [row for row in rows if row in range(0,100)] # update range
+        for row, var in self.rowbuttons.items():
+            if var.get():
+                self.rows.append(row)
+        self.__populate_statistics()
         
     def __update_columns(self):
         #check and prompt for correct input and ouput
@@ -82,6 +86,7 @@ class GUI:
         for column, var in self.checkbuttons.items():
             if var.get():
                 self.columns.append(column)
+        self.__populate_statistics()
                 
     def __update_antenna_filePath(self):
         #check and prompt for correct input
@@ -94,24 +99,43 @@ class GUI:
         # add to FilePath Text
         
     def __populate_statistics(self):
-        print('Populate Power Statistics')
+        #check and prompt for correct input
+        powerStatistics = self.data.power_statistics(self.rows,self.columns)
+        if powerStatistics: 
+            heightConstraint = powerStatistics['heightConstraint']
+            dateConstraint = powerStatistics['dateConstraint']
+            content = [
+                ['Constraint', 'Mean', 'Median', 'Mode'],
+                ['Height', heightConstraint['mean'], heightConstraint['median'],heightConstraint['mode'],],
+                ['Date', dateConstraint['mean'], dateConstraint['median'],dateConstraint['mode']]]
+            for i, row in enumerate(content):
+                for j, element in enumerate(row):
+                    self.text.insert(tk.END, element + ' ')
+                    self.text.grid(row=i, column=j)
+            
         
-    def __populate_correlation_graph():
-        print('populate correlation graph')
+    def __populate_correlation_graph(self):
+        try:
+            self.data.visualize_correlations(self.ax, self.canvas, self.rows, self.columns)
+        except:
+            print('No Visualization')
         
-    def __populate_location_correlation_graph():
-        print('populate location correlation graph')
+    def __populate_location_correlation_graph(self):
+        self.data.visualize_location_impact_on_correlation(self.ax, self.canvas,self.rows, self.columns)
         
                 
 class Data:
-    def __init__(self, serverAddress, databaseName, collectionName, antennaFilePath,paramsFilePath):
+    def __init__(self, serverAddress, databaseName, collectionName):
         self.serverAddress = serverAddress
         self.databaseName = databaseName
         self.collectionName = collectionName
         self.dfDAB = None
     
     def get_df(self):
-        return self.dfDAB
+        if self.dfDAB: return self.dfDAB
+    
+    def get_multiplexes(self):
+        if self.dfDAB : return self.dfDAB['EID'].unique()
     
     def __connect_to_server(self):
         client = MongoClient(self.serverAddress)
@@ -171,10 +195,10 @@ class Data:
     def clear_working_space(self):
         self.dfDAB = None
         
-    def power_statistics(self,multiplex):
+    def power_statistics(self,rows,columns):
         if self.dfDAB:
-            df = self.dfDAB.query(f'EID in {multiplex}')
-            
+            df = self.dfDAB.query(f'EID in {rows}')
+            df = df[columns]
             # mean mode median for multiplex with site height greater than 75
             dfSiteHeight75 = df[df['Site Height'] > 75]
             powerDfSiteHeight75 = dfSiteHeight75['Power (kW)']
@@ -190,23 +214,25 @@ class Data:
                 'mean':powerDfDate2001Onwards.mean(), 'median': powerDfDate2001Onwards.median(), 'mode': powerDfDate2001Onwards.mode().values[0]
             }
             return {
-                'Power statistics site height above 75': powerStatisticsSiteHeightAbove75, 'Power statistics 2001 Onwards': powerStatisticsDate2001Onward
+                'heightConstraint': powerStatisticsSiteHeightAbove75, 'dateConstraint': powerStatisticsDate2001Onward
             }
         else:
             print('No Data in Working Space!')
     
-    def visualize_correlations(self):
+    def visualize_correlations(self, ax, canvas, rows, columns):
         if self.dfDAB:
-            correlation_matrix = self.__compute_correlation_matrix()
-            plt.figure(figsize=(8,6))
-            sns.heatmap(correlation_matrix, cmp='coolwarm', annot=True, fmt='.2f', square=True)
-            plt.title('Correlation between EID Frequency, Block and Label Length')
+            correlation_matrix = self.__compute_correlation_matrix(rows, columns)
+            canvas.delete("all")
+            ax.clear()
+            sns.heatmap(correlation_matrix, cmp='coolwarm', annot=True, fmt='.2f', square=True, ax=ax)
+            ax.set_title('Correlation Statistics')
+            canvas.draw()
         else:
             print('No Data in Working Space!')
     
-    def visualize_location_impact_on_correlation(self):
+    def visualize_location_impact_on_correlation(self, ax, canvas, rows, columns):
         if self.dfDAB:
-            correlation_matrix = self.__compute_correlation_matrix()
+            correlation_matrix = self.__compute_correlation_matrix(rows, columns)
             df = self.dfDAB
             
             #find latitude and longitude of site
@@ -214,24 +240,26 @@ class Data:
             df['Coordinates'] = df['Site'].apply(lambda x: geolocater.geocode(x).point if geolocater.geocode(x) else None)
             df['Latitude'] = df['Coordinates'].apply(lambda x: x.latitude if x.latitude else None)
             df['Longitude'] = df['Longitude'].apply(lambda x: x.longitude if x.longitude else None)
-            
-            plt.figure(figsize=(10,8))
-            sns.heatmap(correlation_matrix, cmap='coolwarm', annot=True, fmt='.2f', square=True)
-            plt.scatter(df['Longitude'], df['Latitude'], marker='x', color='red')
-            plt.title('Affect of Location on Correlation between EID Frequency, Block and Label Length')
-            plt.xlabel('Longitude')
-            plt.ylabel('Latitude')
-            plt.show()
+
+            canvas.delete("all")
+            ax.clear()
+            sns.heatmap(correlation_matrix, cmap='coolwarm', annot=True, fmt='.2f', square=True, ax=ax)
+            ax.scatter(df['Longitude'], df['Latitude'], marker='x', color='red')
+            ax.set_title('Affect of Location on Correlation Statistics')
+            ax.set_xlabel('Longitude')
+            ax.set_ylabel('Latitude')
+            canvas.draw()
             
         else:
             print('No Data in Working Space!')
     
-    def __compute_correlation_matrix(self):
+    def __compute_correlation_matrix(self, rows, columns):
         if self.dfDAB:
-            df = self.dfDAB
+            df = self.dfDAB.query(f"EID in {rows}")
             labels = 'Serv Label1', 'Serv Label2', 'Serv Label3', 'Serv label4','Serv Label10'
             df[labels] = df[[labels]].applymap(len) # apply numerical normilzation to data
             correlation_columns = labels.append('Freq.', 'Block')
+            correlation_columns = [label for label in labels if label in columns] # filters all labels not selected for in GUI
             correlation_matrix = df[correlation_columns].corr()
             return correlation_matrix
         else:
