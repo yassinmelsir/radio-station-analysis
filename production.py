6911bd4388e7c2ad2df385050878d15fbfa6ba9d
Yassin Magdi Elsir <yme@MBP-von-Yassin.fritz.box>
2023-07-18 14:44:40 +0200

    just graphs left to correct

--- production.py
+++ production.py
@@ -12,53 +12,62 @@ class GUI:
         self.data = data
         self.txAntennaFilePath = None
         self.txParamsFilePath = None
-        self.rows = None
-        self.columns = []
+        self.rows = ['C18A', 'C18F', 'C188']
+        self.columns = ['Freq.', 'Block','Serv Label1 ', 'Serv Label2 ', 'Serv Label3 ', 'Serv Label4 ','Serv Label10 ']
         
         #gui init
         self.root = tk.Tk()
         self.root.title('Production')
         
         #gui elements
-        self.figure = plt.figure(figsize=(10,8))
+        self.figure = plt.figure(figsize=(3,2))
         self.ax = self.figure.add_subplot(111)
-        self.canvas = tk.Canvas(self.figure, self.root)
+        self.canvas = FigureCanvasTkAgg(self.figure, master=self.root)
         self.canvas.draw()
         self.text =  tk.Text(self.root)
-        self.txAntennaButton = tk.Button(self.root, text='Find TxAntenna File', command=self.__update_antenna_filePath)
-        self.txParamsButton = tk.Entry(self.root, text='Find TxParams File', command=self.__update_params_filePath)
-        self.load_csv_button = tk.Button(self.root,text='Load From CSV', command=self.__load_from_csv)
-        self.load_database_button = tk.Button(self.root, text='Load From Database', command=self.__load_from_database)
-        self.save_database_button = tk.Button(self.root, text='Save To Database', command=self.__save_to_database)
-        self.correlation_graph_button = tk.Button(self.root, text='Standard Correlation Graph', command=self.__populate_correlation_graph)
-        self.location_correl_graph_button = tk.Button(self.root, text='Location Correlation Graph', command=self.__populate_location_correlation_graph)
+        self.rightFrame = tk.Frame(self.root)
+        self.txAntennaButton = tk.Button(self.rightFrame, text='Find TxAntenna File', command=self.__update_antenna_filePath)
+        self.txAntennaFilePathText =  tk.Text(self.rightFrame, height=5)
+        self.txParamsButton = tk.Button(self.rightFrame, text='Find TxParams File', command=self.__update_params_filePath)
+        self.txParamsFilePathText =  tk.Text(self.rightFrame, height=5)
+        self.load_csv_button = tk.Button(self.rightFrame,text='Load From CSV', command=self.__load_from_csv)
+        self.load_database_button = tk.Button(self.rightFrame, text='Load From Database', command=self.__load_from_database)
+        self.save_database_button = tk.Button(self.rightFrame, text='Save To Database', command=self.__save_to_database)
+        self.correlation_graph_button = tk.Button(self.rightFrame, text='Standard Correlation Graph', command=self.__populate_correlation_graph)
+        self.location_correl_graph_button = tk.Button(self.rightFrame, text='Location Correlation Graph', command=self.__populate_location_correlation_graph)
             
         #gui layout
-        self.canvas.grid(row=0,column=0)
-        self.text.grid(row=1,column=0)
-        widgets = [self.txAntennaButton, self.txParamsButton, self.load_csv_button, self.load_database_button, self.save_database_button, self.row_entry]
-        for i, widget in widgets:
-            widget.grid(row=i, collumn=1)
-        self.rowbuttons = {}
-        for i, row in enumerate(self.data.get_multiplexes()):
-            self.rowbuttons[row] = tk.BooleanVar() 
-            checkbutton = tk.Checkbutton(self.root, text=row, variable=self.rowbuttons[row], command=self.__update_rows)
-            row =  i + len(widgets)
-            checkbutton.grid(row=row, column=1)   
-        self.columnbuttons = {}
-        for i, column in enumerate(self.data.get_df()):
-            self.columnbuttons[column] = tk.BooleanVar() 
-            checkbutton = tk.Checkbutton(self.root, text=column, variable=self.columnbuttons[column], command=self.__update_columns)
-            row = i + len(widgets) + len(self.data.get_multiplexes())
-            checkbutton.grid(row=row, column=1)        
+        for i in range(0,1+1):
+            self.root.grid_rowconfigure(i, weight=1)
+            self.root.grid_columnconfigure(i, weight=1)
+       
         
-        self.roof.mainloop()
+        self.canvas.get_tk_widget().grid(row=0,column=0)
+        self.text.grid(row=1,column=0)
+        self.rightFrame.grid(row=0,column=1, rowspan=2)
+        widgets = [self.txAntennaButton, self.txAntennaFilePathText, self.txParamsButton, self.txParamsFilePathText, self.load_csv_button, self.load_database_button, self.save_database_button, self.correlation_graph_button, self.location_correl_graph_button]
+        for i, widget in enumerate(widgets):
+            widget.pack()
+        self.rowbuttons = {}
+        for i, row in enumerate(self.rows):
+            self.rowbuttons[row] = tk.BooleanVar(value=True) 
+            checkbutton = tk.Checkbutton(self.rightFrame, text=row, variable=self.rowbuttons[row], command=self.__update_rows)
+            checkbutton.pack()
+        self.columnbuttons = {}
+        for i, column in enumerate(self.columns):
+            self.columnbuttons[column] = tk.BooleanVar(value=True) 
+            checkbutton = tk.Checkbutton(self.rightFrame, text=column, variable=self.columnbuttons[column], command=self.__update_columns)
+            checkbutton.pack()     
+                    
+        self.root.mainloop()
+        
         
     def __load_from_csv(self):
         #check and prompt for correct input
         if self.txAntennaFilePath and self.txParamsFilePath:
-            success = self.data.initialize_client_dataset(self.antennaFilePath, self.paramsFilePath)
-            print('Dataset successfully loaded!') if success else print('One or more file paths incorrect!')
+            success = self.data.initialize_client_dataset(self.txAntennaFilePath, self.txParamsFilePath)
+            print('Dataset successfully loaded!') if success else print('Did not Load')
+            if success: self.__populate_statistics
         else:
             print('One or more file paths incorrect!')
         
@@ -66,13 +75,14 @@ class GUI:
         #check and prompt for correct input
         success = self.data.load_from_database()
         print('Dataset successfully loaded!') if success else print('No Data on Files')
+        if success: self.__populate_statistics
     
     def __save_to_database(self):
         #check and prompt for correct input
         success = self.data.save_to_database()
         print('Dataset successfully Saved!') if success else print('Save Unsuccessful!')
     
-    def __update_rows(self,event):
+    def __update_rows(self):
         #check and prompt for correct input
         self.rows.clear()
         for row, var in self.rowbuttons.items():
@@ -83,24 +93,36 @@ class GUI:
     def __update_columns(self):
         #check and prompt for correct input and ouput
         self.columns.clear()
-        for column, var in self.checkbuttons.items():
+        for column, var in self.columnbuttons.items():
             if var.get():
                 self.columns.append(column)
         self.__populate_statistics()
                 
     def __update_antenna_filePath(self):
         #check and prompt for correct input
-        self.txAntennaFilePath = filedialog.askopenfilename()
+        filePath = filedialog.askopenfilename()
+        print(filePath)
         # add to FilePath Text 
+        self.txAntennaFilePath = filePath
+        if filePath:
+            self.txAntennaFilePathText.delete("1.0",tk.END)
+            self.txAntennaFilePathText.insert("1.0", filePath)
         
-    def __update_params_filePath(self, event):
+    def __update_params_filePath(self):
         #check and prompt for correct input
-        self.txParamsFilePath = filedialog.askopenfilename()
-        # add to FilePath Text
+        filePath = filedialog.askopenfilename()
+        print(filePath)
+        # add to FilePath Text 
+        self.txParamsFilePath = filePath
+        if filePath:
+            self.txParamsFilePathText.delete("1.0",tk.END)
+            self.txParamsFilePathText.insert("1.0", filePath)
+        
         
     def __populate_statistics(self):
         #check and prompt for correct input
         powerStatistics = self.data.power_statistics(self.rows,self.columns)
+        print(powerStatistics)
         if powerStatistics: 
             heightConstraint = powerStatistics['heightConstraint']
             dateConstraint = powerStatistics['dateConstraint']
@@ -115,13 +137,10 @@ class GUI:
             
         
     def __populate_correlation_graph(self):
-        try:
-            self.data.visualize_correlations(self.ax, self.canvas, self.rows, self.columns)
-        except:
-            print('No Visualization')
+        self.data.visualize_correlations(self.ax, self.figure, self.canvas, self.rows, self.columns)
         
     def __populate_location_correlation_graph(self):
-        self.data.visualize_location_impact_on_correlation(self.ax, self.canvas,self.rows, self.columns)
+        self.data.visualize_location_impact_on_correlation(self.ax, self.figure, self.canvas, self.rows, self.columns)
         
                 
 class Data:
@@ -147,15 +166,14 @@ class Data:
         if self.dfDAB is not None:
             client, collection = self.__connect_to_server()
             formatted_df = self.dfDAB.to_dict(orient='records')
-            success = None
             try: 
                 collection.insert_many(formatted_df)
-                sucess = True
+                client.close()
+                return True
             except:
                 print('could not save data to collection')
-                sucess = False
-            client.close()
-            return success
+                client.close()
+                return False
         else: print('No Data in Working Space!')
         
     def load_from_database(self):
@@ -163,7 +181,8 @@ class Data:
         results = collection.find()
         formatted_results = list(results)
         if formatted_results: 
-            self.dfDAB = formatted_results
+            self.dfDAB = pd.DataFrame(formatted_results)
+            print(self.dfDAB)
             client.close()
             return True
         else:
@@ -172,31 +191,29 @@ class Data:
             return False
         
         
-    def __cleaning_shaping(dfAntenna,dfParams):
-        dfDAB = dfParams.merge(dfAntenna['NGR', 'Site Height', 'In-Use Ae Ht', 'In-Use ERP Total'], on='Id', how='left')
+    def __cleaning_shaping(self,dfAntenna,dfParams):
+        dfDAB = dfParams.join(dfAntenna[['NGR', 'Site Height', 'In-Use Ae Ht', 'In-Use ERP Total']])
         dfDAB = dfDAB.query("EID in ['C18A', 'C18F', 'C188']")
-        dfDAB = dfDAB['EID', 'Site', 'Site Height', 'In-Use Ae Ht', 'In-Use ERP Total', 'Freq', 'Block', 'Serv Label1', 'Serv Label2', 'Serv Label3', 'Serv label4','Serv Label10']
+        dfDAB = dfDAB[['EID', 'Site', 'Freq.', 'Block', 'Serv Label1 ', 'Serv Label2 ', 'Serv Label3 ', 'Serv Label4 ','Serv Label10 ','Site Height', 'In-Use Ae Ht', 'In-Use ERP Total']]
         dfDAB = dfDAB.rename({'In-Use Ae Ht': 'Aerial height (m)', 'In-Use ERP Total': 'Power (kW)'})
         return dfDAB
 
     def initialize_client_dataset(self, antennaFilePath, paramsFilePath):
         #load from file
-        try:
-            dfAntenna = pd.read_csv(antennaFilePath)
-            dfParams = pd.read_csv(paramsFilePath)
-        except:
-            return False
+        dfAntenna = pd.read_csv(antennaFilePath)
+        dfParams = pd.read_csv(paramsFilePath, encoding='latin-1')
         #shape and clean
         dfDAB = self.__cleaning_shaping(dfAntenna, dfParams)
         #load into workingspace
         self.dfDAB = dfDAB
-        if self.dfDAB: return True
+        print(self.dfDAB)
+        return True if self.dfDAB.shape[0] > 0 else False
     
     def clear_working_space(self):
         self.dfDAB = None
         
     def power_statistics(self,rows,columns):
-        if self.dfDAB:
+        if self.dfDAB.shape[0] > 0:
             df = self.dfDAB.query(f'EID in {rows}')
             df = df[columns]
             # mean mode median for multiplex with site height greater than 75
@@ -219,19 +236,20 @@ class Data:
         else:
             print('No Data in Working Space!')
     
-    def visualize_correlations(self, ax, canvas, rows, columns):
-        if self.dfDAB:
+    def visualize_correlations(self, ax, figure, canvas, rows, columns):
+        if self.dfDAB.shape[0] > 0:
             correlation_matrix = self.__compute_correlation_matrix(rows, columns)
-            canvas.delete("all")
+            figure.clf()
             ax.clear()
-            sns.heatmap(correlation_matrix, cmp='coolwarm', annot=True, fmt='.2f', square=True, ax=ax)
+            breakpoint()
+            sns.heatmap(correlation_matrix, cmap='coolwarm', annot=True, fmt='.2f', square=True, ax=ax)
             ax.set_title('Correlation Statistics')
             canvas.draw()
         else:
             print('No Data in Working Space!')
     
-    def visualize_location_impact_on_correlation(self, ax, canvas, rows, columns):
-        if self.dfDAB:
+    def visualize_location_impact_on_correlation(self, ax, figure, canvas, rows, columns):
+        if self.dfDAB.shape[0] > 0:
             correlation_matrix = self.__compute_correlation_matrix(rows, columns)
             df = self.dfDAB
             
@@ -241,7 +259,7 @@ class Data:
             df['Latitude'] = df['Coordinates'].apply(lambda x: x.latitude if x.latitude else None)
             df['Longitude'] = df['Longitude'].apply(lambda x: x.longitude if x.longitude else None)
 
-            canvas.delete("all")
+            figure.clf()
             ax.clear()
             sns.heatmap(correlation_matrix, cmap='coolwarm', annot=True, fmt='.2f', square=True, ax=ax)
             ax.scatter(df['Longitude'], df['Latitude'], marker='x', color='red')
@@ -254,11 +272,11 @@ class Data:
             print('No Data in Working Space!')
     
     def __compute_correlation_matrix(self, rows, columns):
-        if self.dfDAB:
+        if self.dfDAB.shape[0] > 0:
             df = self.dfDAB.query(f"EID in {rows}")
-            labels = 'Serv Label1', 'Serv Label2', 'Serv Label3', 'Serv label4','Serv Label10'
-            df[labels] = df[[labels]].applymap(len) # apply numerical normilzation to data
-            correlation_columns = labels.append('Freq.', 'Block')
+            labels = ['Serv Label1 ', 'Serv Label2 ', 'Serv Label3 ', 'Serv Label4 ', 'Serv Label10 ']
+            df[labels] = df[labels].applymap(len) # apply numerical normilzation to data
+            correlation_columns = labels + ['Freq.', 'Block']
             correlation_columns = [label for label in labels if label in columns] # filters all labels not selected for in GUI
             correlation_matrix = df[correlation_columns].corr()
             return correlation_matrix
@@ -269,3 +287,6 @@ class Data:
 serverAddress = 'mongodb://127.0.0.1:27017/?directConnection=true&serverSelectionTimeoutMS=2000&appName=mongosh+1.10.1'
 databaseName = 'test'
 collection  = 'DAB'
+
+data = Data(serverAddress, databaseName, collection)
+gui = GUI(data)
